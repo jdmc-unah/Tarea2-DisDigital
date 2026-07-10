4819b0806f6d9471f15ddb7bd9ca1b0518247362
Bryan01Cruz <bcruzi@unah.hn>
2026-07-10 15:26:24 -0600

    Métodos controller.
    Creación de métodos para utilizar en los endpoints get y post.

---
+++ src/controllers/solicitud.controller.js
@@ -0,0 +1,43 @@
+import SolicitudModel from "../models/solicitud.model.js"
+import {jsonResponse} from "../helpers/jsonResponse.js"
+
+export const crearSolicitud = async (req,res) => {
+    const payload = req.validateBody
+
+    const nueva = await SolicitudModel.create(payload)
+
+    res.status(201).json(jsonResponse({
+        status: 201,
+        message: "La solicitud se creo correctamente.",
+        data: nueva
+    }))
+}
+
+export const obtenerSolicitudes = async (req,res) => {
+    const {estado} = req.query
+
+    const lista = await SolicitudModel.getAll(estado)
+    res.json(jsonResponse({
+        message: "Listado de solicitudes.",
+        data: lista
+    }))
+}
+
+export const obtenerSolicitudPorId = async (req, res) => {
+    const {id} = req.params
+
+    const solicitud = await SolicitudModel.getById(id)
+
+    if(!solicitud){
+        return res.status(404).json(jsonResponse({
+            status: 404,
+            message: "La solicitud no a sido encontrada.",
+            data:null
+        }))
+    }
+
+    res.json(jsonResponse({
+        message: "Datos de la solicitud.",
+        data: solicitud
+    }))
+}
